021092d4009db0fa9bdf585fda1b9475c3bf58d8
filip223115 <filip.postolov@students.finki.ukim.mk>
2025-05-29 21:13:32 +0200

    Initial commit

---
+++ src/main/java/com/shops/customshop/CustomShopApplication.java
@@ -0,0 +1,13 @@
+package com.shops.customshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shops/customshop/CustomShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shops.customshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
